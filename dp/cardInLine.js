0f81ba1e0a55e0a9f606d02a5a9c824acf123612
justin5562587 <justin5562587@gmail.com>
2022-05-15 21:31:29 +0800

    exercise dp

---
+++ dp/cardInLine.js
@@ -0,0 +1,35 @@
+// 问题：
+// 给定一个整型数组arr来表示所有扑克牌
+// 玩家a 和 玩家b 每次只能从数组的左右两边拿走一张牌
+// 问先手和后手的人 谁能获取最后的胜利
+
+const win1 = (arr) => {
+    if (!arr) {
+        return -1;
+    }
+
+    return Math.max(
+        first(arr, 0, arr.length - 1),
+        second(arr, 0, arr.length - 1)
+    );
+};
+
+const first = (arr, L, R) => {
+    if (L === R) {
+        return arr[L];
+    }
+
+    let score1 = arr[L] + second(arr, L + 1, R);
+    let score2 = arr[R] + second(arr, L, R - 1);
+    return Math.max(score1, score2);
+};
+
+const second = (arr, L, R) => {
+    if (L === R) {
+        return 0;
+    }
+
+    let score1 = first(arr, L + 1, R);
+    let score2 = first(arr, L, R  - 1);
+    return Math.min(score1, score2);
+};
--- dp/convertSttringToLetter.js
+++ dp/convertSttringToLetter.js
@@ -1,52 +1,44 @@
+// 问题：
 // 1表示A 2表示B 3表示C .... 26表示Z
 // 所以111可以表示AAA AK KA
 // 请问提供一个数字给你，有多少种转化方式
 
 const convertStringToLetter = (numStr) => {
-    let ans = [];
     return process(numStr, 0);
 };
 
-// 0...index已经被尝试过了
+// 0...index-1已经被尝试过了
 // index...numStr.length-1 还没有被尝试过
 // 如果走到了index===numStr.length 说明所有index已经被尝试完成 有效解+1
 const process = (numStr, index) => {
-    // 超过numStr可用范围 无效解
-    if (index > numStr.length) {
-        return 0;
-    }
     // 表示已经用完了所有字符 认为是一个转发方法
     if (index === numStr.length) {
         return 1;
     }
 
-    // 下方表示还没有检查完毕所有numStr
-
-    if (numStr.substr(index, 1) === '0') {
+    // 单个位置的0无法自我解析
+    if (numStr[index] === '0') {
         return 0;
     }
 
-    // 如果当前数字字符为1
-    if (numStr.substr(index, 1) === '1') {
-        return process(numStr, index + 1) + process(numStr, index + 2);
+    let ways = process(numStr, index + 1);
+    if (index + 1 < numStr.length && parseInt(numStr.substr(index, 2)) < 27) {
+        ways += process(numStr, index + 2);
     }
- 
-    // 如果当前数字字符为2
-    if (numStr.substr(index, 1) === '2') {        
-        let nextIndexChar = numStr.substr(index + 1, 1);
-        if (nextIndexChar && Number(nextIndexChar) <= 6) {
-            return process(numStr, index + 1) + process(numStr, index + 2);
-        }
-
-        return process(numStr, index + 1);
-    }
-
-    // 如果当前数字字符不是1和2
-    return process(numStr, index + 1);
+    
+    return ways;
 };
 
 convertStringToLetter('111')
 
-const dp = () => {
+const dp = (numStr) => {
+    if (numStr == null || numStr == '') {
+        return 0;
+    }
+
+    return dpProcess(0);
+};
+
+const dpProcess = () => {
 
 };
