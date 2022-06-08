c792e679431e5617560eaaffa1a6802324bb7907
justin5562587 <justin5562587@gmail.com>
2022-06-08 15:10:57 +0800

    exercise linkedlist

---
+++ linkedList/deleteGivenNode.js
@@ -0,0 +1,62 @@
+// 删除单链表中所有value为nodeValue的节点
+const deleteSingleLinkedListNode = (head, nodeValue) => {
+    if (head === null) {
+        return;
+    }
+ 
+    // head来到第一个不需要删的位置
+    while (head !== null) {
+        if (head.value !== nodeValue) {
+            break;
+        }
+        head = head.next;
+    }
+
+    let pre = head;
+    let cur = head;
+    while (cur.value !== null) {
+        if (cur.value === nodeValue) {
+            pre.next = cur.next;
+        } else {
+            pre = cur;
+        }
+        cur = cur.next;
+    }
+
+    return head;
+};
+
+// 删除双链表中所有value为nodeValue的节点
+const deleteDoubleLinkedListNode = (head, nodeValue) => {
+    if (head === null) {
+        return;
+    }
+
+    // 找到第一个不被删除的节点，将它作为头部，并设置他的last为null
+    while (head !== null) {
+        if (head.value !== nodeValue) {
+            break;
+        }
+        head = head.next;
+    }
+    head.last = null;
+
+    let pre = head;
+    let cur = head;
+    while (cur !== null) {
+        if (cur.value === nodeValue) {
+            if (cur.next !== null) {
+                pre.next = cur.next;
+                cur.next.last = pre;
+            } else {
+                pre.next = null;
+            }
+        } else {
+            pre = cur;
+        }
+
+        cur = cur.next;
+    }
+
+    return head;
+};
---
+++ linkedList/linkedNode.js
@@ -0,0 +1,14 @@
+class SNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class DNode {
+    constructor() {
+        this.value = value;
+        this.last = null;
+        this.next = null; 
+    }
+}
