2297903ddbbe39d2dc003371889a3f1db7d2111c
justin5562587 <justin5562587@gmail.com>
2022-06-10 15:05:24 +0800

    exercise add-two-numbers

--- leetcode/addTwoNumbers.js
+++ leetcode/addTwoNumbers.js
@@ -1,8 +1,6 @@
 // https://leetcode.com/problems/add-two-numbers/
 // Add Two Numbers
 
-// 解决方案1：
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -15,6 +13,39 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
- var addTwoNumbers = function(l1, l2) {
-    
+var addTwoNumbers = function(l1, l2) {
+    let head = null;
+    let current = null;
+    let carry = 0;
+    let sum;
+    let node;
+
+    while (l1 || l2) {
+        let val1 = l1 ? l1.val : 0;
+        let val2 = l2 ? l2.val : 0;
+        sum = val1 + val2 + carry;
+        // 加完进位后进位清零
+        carry = 0;
+        if (sum >= 10) {
+            carry = 1;
+            sum = sum % 10;
+        }
+        if (head === null) {
+            head = new ListNode(sum);
+            current = head;
+        } else {
+            node = new ListNode(sum);
+            current.next = node;
+            current = node;
+        }
+
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+    }
+
+    if (carry !== 0) {
+        current.next = new ListNode(1);
+    }
+
+    return head;
 };
