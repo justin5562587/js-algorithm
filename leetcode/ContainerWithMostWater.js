5f5ecd96f1b087fd77d0a4345304518d9862c168
justin5562587 <justin5562587@gmail.com>
2022-06-11 21:41:00 +0800

    fix Container With Most Water

--- leetcode/ContainerWithMostWater.js
+++ leetcode/ContainerWithMostWater.js
@@ -5,6 +5,25 @@
  * @param {number[]} height
  * @return {number}
  */
- var maxArea = function(height) {
+var maxArea = function(height) {
+    if (!height || height.length < 2) {
+        return 0;
+    }
     
+    let L = 0;
+    let R = height.length - 1;
+    let ans = (R - L) * Math.min(height[L], height[R]);
+    while (L < R) {
+        ans = Math.max(
+            ans,
+            (R - L) * Math.min(height[L], height[R])
+        );
+        if (height[L] <= height[R]) {
+            L++;
+        } else {
+            R--;
+        }
+    }
+    
+    return ans;
 };
