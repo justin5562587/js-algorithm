ab6283478ce326c04104aee60d935d508ea0a0bb
justin5562587 <justin5562587@gmail.com>
2022-06-12 23:05:38 +0800

    fix Merge Two Sorted Lists

--- leetcode/Merge Two Sorted Lists.js
+++ leetcode/Merge Two Sorted Lists.js
@@ -14,5 +14,21 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    
+    let dummy = new ListNode('dummy');
+    let head = dummy;
+
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            dummy.next = list1;
+            list1 = list1.next;
+        } else {
+            dummy.next = list2;
+            list2 = list2.next;
+        }
+        dummy = dummy.next;
+    }
+
+    dummy.next = list1 || list2;
+
+    return head.next;
 };
