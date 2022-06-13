c36253628abc55f4ac348721c3c20b16f4f03539
justin5562587 <justin5562587@gmail.com>
2022-06-14 00:02:32 +0800

    fix Swap Nodes in Pairs

--- leetcode/24. Swap Nodes in Pairs.js
+++ leetcode/24. Swap Nodes in Pairs.js
@@ -12,26 +12,49 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var swapPairs = function(head) {
-    if (!head) {
-        return null;
+ var swapPairs = function(head) {
+    if (!head || (head && head.next === null)) {
+        return head;
     }
 
     let dummy = new ListNode('dummy', head);
-    let slow = dummy;
-    let fast = dummy;
-    while (fast != null) {
+    let index = dummy;
+    let pairFirst = null; // 需要交换的两个节点的前一个
+    let pairSecond = null; // 需要交换的两个节点的后一个
+    let pairHead = null; // 需进行交换的两个节点的再前面一个
+    let pairTail = null; // 需进行交换的两个节点的再后面一个
 
+    let time = 0;
+    while (index !== null) {
+        console.log(`${++time} index=${index.val}`);
+        // 走第一步
+        if (index.next === null) {
+            break;
+        }
+        pairHead = index;
+        index = index.next;
+        pairFirst = index;
+
+        // 走第二步
+        if (index.next === null) {
+            break;
+        }
+        index = index.next;
+        pairSecond = index;
+        pairTail = index.next;
+
+        // 交换逻辑
+        // l1 l2 l3 l4
+        // l1.next = l3
+        // l2.next = l4
+        // l3.next = l2
+        // 走完了两步，进行交换
+        pairHead.next = pairSecond;
+        pairFirst.next = pairTail;
+        pairSecond.next = pairFirst;
+
+        index = pairFirst;
     }
 
     return dummy.next;
-};
-
-const swapNode = (node1, node2) => {
-    // l1 l2 l3 l4
-    // l1.next = l3
-    // l2.next = l4
-    // l3.next = l2
-
-    // 
 };
