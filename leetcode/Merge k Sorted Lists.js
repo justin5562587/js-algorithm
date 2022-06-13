eb13bef1f956adb0ddc1975873fbf096e3a58044
justin5562587 <justin5562587@gmail.com>
2022-06-13 14:20:45 +0800

    fix Merge k Sorted Lists

--- leetcode/Merge k Sorted Lists.js
+++ leetcode/Merge k Sorted Lists.js
@@ -13,7 +13,41 @@
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
+    if (!lists) {
+        return lists;
+    }
+
+    if (lists.length === 1) {
+        return lists[0];
+    }
+
+    let dummy = new ListNode('dummy');
+    let head = dummy;
+    for (let list of lists) {
+        dummy = mergeTwoLists(list, dummy);
+    }
+
+    return head.next;
+};
+
+const mergeTwoLists = (l1, l2) => {
+    let dummy = new ListNode('dummy');
+    let head = dummy;
     
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            dummy.next = l1;
+            l1 = l1.next;
+        } else {
+            dummy.next = l2;
+            l2 = l2.next;
+        }
+        dummy = dummy.next;
+    }
+
+    dummy.next = l1 || l2;
+
+    return head.next;
 };
 
 
