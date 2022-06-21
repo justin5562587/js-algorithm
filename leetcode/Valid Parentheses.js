753c460bd5405dc260de536f3f176583f9d0c47a
justin5562587 <justin5562587@gmail.com>
2022-06-21 17:55:18 +0800

    fix Valid Parentheses

--- leetcode/Valid Parentheses.js
+++ leetcode/Valid Parentheses.js
@@ -6,5 +6,39 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack = new Stack();
+    for (let c of s) {
+        if (c === '(') {
+            stack.push(')');
+        } else if (c === '[') {
+            stack.push(']');
+        } else if (c === '{') {
+            stack.push('}');
+        } else if (stack.isEmpty() || stack.pop() !== c) {
+            return false;
+        }
+    }
+
+    return stack.isEmpty();
 };
+
+class Stack {
+    constructor() {
+        this.content = [];
+    }
+
+    isEmpty() {
+        return this.content.length === 0;
+    }
+
+    push(element) {
+         this.content.push(element);
+    }
+
+    pop() { 
+        if (this.content.length > 0) {
+            return this.content.pop();
+        }
+        return null;
+    }
+}
