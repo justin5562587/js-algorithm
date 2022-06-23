5a0f331fb2682ac86c3beff0d8ad0057c7ca4d17
justin5562587 <justin5562587@gmail.com>
2022-06-23 17:19:16 +0800

    learn Search in Rotated Sorted Array

--- leetcode/Top Interview Questions/33. Search in Rotated Sorted Array.js
+++ leetcode/Top Interview Questions/33. Search in Rotated Sorted Array.js
@@ -7,5 +7,40 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    
+    if (!nums) return -1;
+
+    let L = 0;
+    let R = nums.length - 1;
+    let mid;
+    while (L <= R) {
+        mid = L + ((R - L) >> 1);
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[L] <= nums[mid]) {
+            if (nums[L] <= target && target < nums[mid]) {
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        } else {
+            if (nums[R] >= target && target > nums[mid]) {
+                L = mid + 1;
+            } else {
+                R = mid - 1;
+            }
+        }
+    }
+
+    return -1;
 };
+
+search([3,1], 1);
+
+const getMid = ( arr) => {
+    let L = 0;
+    let R = arr.length - 1;
+    return L + ((R - L) >> 1);
+};
+getMid([1,1,1,1,1,1]);
