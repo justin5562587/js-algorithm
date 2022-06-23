98cb55b5ba64aca67e47e2c7fec5eaf39ebd31a2
justin5562587 <justin5562587@gmail.com>
2022-06-23 16:09:50 +0800

    refactor dir

---
+++ leetcode/Top Interview Questions/28. Implement strStr().js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/implement-strstr/
+// 28. Implement strStr()
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    
+};
---
+++ leetcode/Top Interview Questions/33. Search in Rotated Sorted Array.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+// 33. Search in Rotated Sorted Array
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+};
