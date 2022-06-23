c9ff8c507ed414b6092fcdec6021f5286be6ad18
justin5562587 <justin5562587@gmail.com>
2022-06-23 11:12:41 +0800

    fix generateParenthesis

--- leetcode/Generate Parentheses.js
+++ leetcode/Generate Parentheses.js
@@ -6,5 +6,34 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-    
+    if (n < 1) return [];
+
+    let ans = [];
+    process(0, '', n * 2, ans);
+    return ans;
+};
+
+const process = (index, current, n, ans) => {
+    if (index === n) {
+        if (isValid(current)) {
+            ans.push(current);
+        }
+        return;
+    }
+
+    process(index + 1, current + '(', n, ans);
+    process(index + 1, current + ')', n, ans);
+};
+
+const isValid = (str) => {
+    let arr = [];
+    for (let c of str) {
+        if (c === '(') {
+            arr.push(')');
+        } else if (arr.length === 0 || arr.pop() !== c) {
+            return false;
+        }
+    }
+
+    return arr.length === 0;
 };
