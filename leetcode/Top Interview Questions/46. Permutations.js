da2b1f2c7f87efd86c61754f59aaf77ece6618c6
justin5562587 <justin5562587@gmail.com>
2022-07-14 18:10:11 +0800

    fix permutations

--- leetcode/Top Interview Questions/46. Permutations.js
+++ leetcode/Top Interview Questions/46. Permutations.js
@@ -6,6 +6,37 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// 交换法处理
 var permute = function(nums) {
-    
+    if (!nums) return [];
+    if (nums.length === 1) return [nums];
+
+    let ans = [];
+    process(0, nums, ans);
+    return ans;
 };
+
+const process = (index, nums, ans) => {
+    console.log(index, nums)
+    if (index === nums.length) {
+        ans.push(nums.concat());
+        return;
+    }
+
+    for (let i = index; i < nums.length; i++) {
+        swap(nums, index, i);
+        process(index + 1, nums, ans);
+        swap(nums, index, i);
+    }
+};
+
+const swap = (arr, a, b) => {
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+};
+
+// test
+permute([1,2,3]);
+
+// 字典序 处理
